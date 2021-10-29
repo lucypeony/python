b31e2f97f4503aff4c3a0a95be5c4f53e6ba75b5
lucypeony <lucypeony@hotmail.com>
2021-10-29 14:46:04 +0800

    Update 去掉机号.py
    极域里的机号和原文件名之间有一个下划线，利用下划线拆分原文件名，然后重命名

--- 去掉机号.py
+++ 去掉机号.py
@@ -7,7 +7,22 @@ import re
 path='./'
 for file in os.listdir(path):
 	#file can be seen as a string, can use regex on it 
-	if re.search('_', file):
-		print(file)
+	if re.search('_', file) and file[0].isdigit():
+                dash_index = file.index('_')
+                new_name = file[dash_index+1::]    #把文件名里的机号和下换线去掉
+                #重命名文件
+                #拼接获取绝对路径
+                src = os.path.join(os.getcwd(), file)
+                dst = os.path.join(os.getcwd(), new_name)
+
+                #使用rename函数
+                try:
+                        os.rename(src,dst)
+                except Exception as e:
+                        print(e)
+                        print('rename file fail \r\n')
+                else:
+                        print('rename file succeed\r\n')
+                print(file + " " + new_name)
 	else:
 		print("not found")
